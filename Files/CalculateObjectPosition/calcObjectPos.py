e621d02edd80f5c82923dd943981f93a0f58039a
Jacob Mose Hansen <Jacobmosehansen@gmail.com>
2015-10-09 12:10:28 +0200

    added objectposition class

--- Files/CalculateObjectPosition/calcObjectPos.py
+++ Files/CalculateObjectPosition/calcObjectPos.py
@@ -3,41 +3,43 @@
 from planar import Affine
 from planar import Vec2
 
-def calculateObjectPosition(x_pixel, y_pixel):
+class ObjectPosition:
 
-	# x_pixel offset = +5 and y_pixel offset = -5
-	# this is not done and should be set somewhere
+	def calculateObjectPosition(x_pixel, y_pixel):
 
-	# Robots world coordinates seen from camera frame
-	x_coordinate_robot_position = 34.7
-	y_coordinate_robot_position = 33.9
+		# x_pixel offset = +5 and y_pixel offset = -5
+		# this is not done and should be set somewhere
 
-	# Resolution in cm per pixel
-	resolution_width = 70.0/611.0
-	resolution_height = 39.5/344.0	
+		# Robots world coordinates seen from camera frame
+		x_coordinate_robot_position = 34.7
+		y_coordinate_robot_position = 33.9
 
-	# Robots position seen from camera frame
-	T1 = Affine.translation((x_coordinate_robot_position, y_coordinate_robot_position))
+		# Resolution in cm per pixel
+		resolution_width = 70.0/611.0
+		resolution_height = 39.5/344.0	
 
-	# Robots rotating frame opposite to camera frame - degrees
-	rot = Affine.rotation(180)
+		# Robots position seen from camera frame
+		T1 = Affine.translation((x_coordinate_robot_position, y_coordinate_robot_position))
 
-	# Getting robot frame
-	robot_frame = T1*rot
+		# Robots rotating frame opposite to camera frame - degrees
+		rot = Affine.rotation(180)
 
-	# Actual distance seen from camera frame
-	actual_distance_width = resolution_width*x_pixel
-	actual_distance_height = resolution_height*y_pixel
+		# Getting robot frame
+		robot_frame = T1*rot
 
-	# Object position seen from camera frame
-	P_yx_camera_frame = Vec2(actual_distance_height, actual_distance_width)
+		# Actual distance seen from camera frame
+		actual_distance_width = resolution_width*x_pixel
+		actual_distance_height = resolution_height*y_pixel
 
-	# Object position seen from the robot frame
-	P_xyz_robot_frame = ~robot_frame*P_yx_camera_frame
+		# Object position seen from camera frame
+		P_yx_camera_frame = Vec2(actual_distance_height, actual_distance_width)
 
-	return P_xyz_robot_frame
+		# Object position seen from the robot frame
+		P_xyz_robot_frame = ~robot_frame*P_yx_camera_frame
 
-print(calculateObjectPosition(40, 295))
+		return P_xyz_robot_frame
+
+
 
 
 
